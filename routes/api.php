2bddf451502ecf5313ea56b87a59d8e608def778
quanghung97 <hungcan1997@gmail.com>
2018-10-05 12:29:26 +0700

    middleware demo auth in api.php

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/info', 'TestController@info');
+Route::get('/info', 'TestController@info')->middleware('auth');
